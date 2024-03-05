2b052e3e62f4b27b8616fc2ff97e7e72d4c1a12e
YarynaFialko <yarynafialko@gmail.com>
2024-03-05 20:46:02 +0200

    Updated Helix Controls

--- Assets/Scripts/HelixRotation.cs
+++ Assets/Scripts/HelixRotation.cs
@@ -3,29 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelixRotation : MonoBehaviour {
-
-    public float rotationSpeed = 60f;
+    private float rotationSpeed = 60f;
+    private float inertiaDuration = 0.3f;
     private bool isRotating = false;
     private Vector3 lastMousePosition;
+    private float dpiScale;
+    private float rotationVelocity;
+    private float lastRotationAmount;
+    private float inertiaCounter;
+    private const float standardDPI = 96f;
+
+    private void Start()
+    {
+        if (Screen.dpi < 1) dpiScale = 1;
+        else dpiScale = Mathf.Log(Screen.dpi / standardDPI + 1);
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
             isRotating = true;
             lastMousePosition = Input.mousePosition;
+            inertiaCounter = 0;
         }
 
         if (Input.GetMouseButtonUp(0)) {
             isRotating = false;
+            inertiaCounter = inertiaDuration;
         }
 
         if (isRotating) {
             Vector3 mouseDelta = Input.mousePosition - lastMousePosition;
-            float rotationAmount = mouseDelta.x * rotationSpeed * Time.deltaTime;
-
+            float rotationAmount = -mouseDelta.x * rotationSpeed * dpiScale * Time.deltaTime;
+            lastRotationAmount = rotationAmount;
             transform.Rotate(Vector3.up, rotationAmount);
-
             lastMousePosition = Input.mousePosition;
+        }
+        else if (inertiaCounter > 0) {
+            transform.Rotate(Vector3.up, lastRotationAmount);
+            lastRotationAmount *= inertiaCounter / inertiaDuration;
+            inertiaCounter -= Time.deltaTime;
         }
     }
 }
